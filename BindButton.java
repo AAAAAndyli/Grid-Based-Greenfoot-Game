910ac705ac3a0b3f7452a4cec10ac10e92665f2c
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2025-01-15 13:43:21 -0500

    wip bind buttons

--- BindButton.java
+++ BindButton.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.Arrays;
 
 /**
  * Write a description of class BindButton here.
@@ -8,15 +9,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class BindButton extends Button
 {
-    private SuperTextBox textBox;
+    private SuperTextBox textBox, bindBox;
     private String text;
-    private boolean added = false;
+    private String key;
+    private boolean added, pressed = false;
     
     public BindButton(String file, double sizeMulti, String txt)
     {
         super(file, sizeMulti);
         text = txt;
         textBox = new SuperTextBox(text, new Font("Arial", false, false, 20), 180);
+        bindBox = new SuperTextBox("Press" + text, new Font("Arial", false, false, 20), 190);
     }
     
     /**
@@ -25,10 +28,24 @@ public class BindButton extends Button
      */
     public void act()
     {
+        //add corresponding textBox once the button exists
         if(!added && textBox != null){
             getWorld().addObject(textBox, getX() - 250, getY());
             added = true;
         }
-        super.act();
+        pressed = checkButton();
+        
+        if(pressed){
+            System.out.println(Arrays.toString(bindBox.getText()));
+            getWorld().addObject(bindBox, getWorld().getWidth() / 2, getWorld().getHeight() - 50);
+        }
+        key = Greenfoot.getKey();
+        if(Greenfoot.isKeyDown("a")){
+            System.out.println(bindBox.getWorld());
+        }
+        if(bindBox.getWorld() != null && key != null){
+            System.out.println(111);
+            bindBox.removeSelf();
+        }
     }
 }
