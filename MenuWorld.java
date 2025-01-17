f120c47571e4655b8ab19d09409338ca6f763cd4
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2025-01-16 21:16:04 -0500

    remove github merging stuff

--- MenuWorld.java
+++ MenuWorld.java
@@ -19,14 +19,10 @@ public class MenuWorld extends World
         super(1080, 720, 1); 
         setBackground("images/menuPlaceholder.png");
         
-<<<<<<< HEAD
         //load save file for data
         SaveFile.loadFile();
         
         Label playLabel = new Label("Play", 40);
-=======
-        
->>>>>>> 2f7a8aef63b4388a1a4f1e5abe0ecd7e84c0c770
         addObject(playLabel, 900, 200);
         Label settingsLabel = new Label("Settings", 40);
         addObject(settingsLabel, 900, 400);
