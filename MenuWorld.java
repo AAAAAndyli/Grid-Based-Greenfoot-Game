1981097a0a22a55cbc2abd5387c83fb822787128
JustYe <341153946@gapps.yrdsb.ca>
2025-01-20 15:59:52 -0500

    This setup makes it easier to debug and play the game

--- MenuWorld.java
+++ MenuWorld.java
@@ -128,13 +128,13 @@ public class MenuWorld extends World
     
     public void resetStats()
     {
-        SaveFile.setInfo("money", 0);
+        SaveFile.setInfo("money", 1000); // 0
         SaveFile.setInfo("health", 15);
         SaveFile.setInfo("maxHealth", 15);
         SaveFile.setInfo("level", 0);
-        SaveFile.setInfo("damage", 1);
-        SaveFile.setInfo("hasBomb", 0);
-        SaveFile.setInfo("hasMissile", 0);
-        SaveFile.setInfo("hasSpread", 0);
+        SaveFile.setInfo("damage", 1); // 0
+        SaveFile.setInfo("hasBomb", 1); // 0
+        SaveFile.setInfo("hasMissile", 1); // 0
+        SaveFile.setInfo("hasSpread", 1); // 0
     }
 }
