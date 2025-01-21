b9437cc4d3ee93a6e861443797209a07e4abb42d
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2025-01-21 09:14:36 -0500

    added tips to death screen

--- GameOver.java
+++ GameOver.java
@@ -10,6 +10,13 @@ public class GameOver extends World
 {
     private int actTimer = 0;
     private Transition transision = new Transition(true);
+    String[] tips = new String[]{
+        "Tip: A successful parry will give HP!",
+        "Tip: Using a bomb or missile while \njumping provides extra height!",
+        "Tip: Jumping again after slamming \ndown provides extra height!",
+        "Tip: Bomb is a good AoE weapon!"
+    };
+    private Label tipLabel;
     /**
      * Constructor for objects of class GameOver.
      * 
@@ -19,6 +26,8 @@ public class GameOver extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1080, 720, 1); 
         setBackground("GameOver.png");
+        tipLabel = new Label(tips[Greenfoot.getRandomNumber(tips.length)], 40);
+        addObject(tipLabel, getWidth() / 2, getHeight() / 2 + 150);
     }
     public void act()
     {
