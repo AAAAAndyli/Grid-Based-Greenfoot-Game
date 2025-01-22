603379d4ca505037e265bb35b3d78aef5781499a
Incxndiary <349070870@gapps.yrdsb.ca>
2025-01-21 22:00:00 -0500

    Added tutorialavatar again

--- NextWorld.java
+++ NextWorld.java
@@ -1,43 +1,62 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class KillBorder here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Write a description of class NextWorld here.
  */
-public class NextWorld extends CollisionTrigger
-{   
+public class NextWorld extends CollisionTrigger {
+    private String[]portal = {
+        "Remember to press space to enter the portal"
+    };
     private Transition transition = new Transition(true);
-    public NextWorld(String type, int rotations, int xPosition, int yPosition, int triggerNumber)
-    {
+    private boolean avatarSpawned = false; //Checks it an avatar is already spawned 
+
+    public NextWorld(String type, int rotations, int xPosition, int yPosition, int triggerNumber) {
         this(type, rotations, false, null, xPosition, yPosition, triggerNumber);
     }
-    public NextWorld(String type, int rotations, boolean isButton, MapMaker mapMaker, int triggerNumber)
-    {
+
+    public NextWorld(String type, int rotations, boolean isButton, MapMaker mapMaker, int triggerNumber) {
         this(type, rotations, isButton, mapMaker, 0, 0, triggerNumber);
     }
-    public NextWorld(String type, int rotations, boolean isButton, MapMaker mapMaker, int xPosition, int yPosition, int triggerNumber)
-    {
-        super(type,rotations,isButton,mapMaker,xPosition,yPosition, triggerNumber);
+
+    public NextWorld(String type, int rotations, boolean isButton, MapMaker mapMaker, int xPosition, int yPosition, int triggerNumber) {
+        super(type, rotations, isButton, mapMaker, xPosition, yPosition, triggerNumber);
     }
-    public void addedToWorld(World world)
-    {
+
+    public void addedToWorld(World world) {
         super.addedToWorld(world);
         getImage().scale(50, 100);
     }
-    public void act()
-    {
+
+    public void act() {
         super.act();
+        checkPlayerProximity(); 
     }
-    public void activateTrigger()
-    {
-        if (isTouching(Player.class) && Greenfoot.isKeyDown("space")) 
-        {
+
+    /**
+     * Spawns a new TutorialAvatar if the player is close to this object.
+     */
+    private void checkPlayerProximity() {
+        //Checks if the player is touching the portal
+        Player player = (Player) getOneIntersectingObject(Player.class);
+
+        if (player != null && !avatarSpawned) {
+            
+            World current = getWorld();
+            //If the CURRENT AND ONLY world is Tutorial....
+            if (current instanceof Tutorial) {
+                
+                TutorialAvatar tutorialAvatar = new TutorialAvatar(portal);
+                getWorld().addObject(tutorialAvatar, 1080, 650);
+                avatarSpawned = true; 
+            }
+        }
+    }
+
+    public void activateTrigger() {
+        if (isTouching(Player.class) && Greenfoot.isKeyDown("space")) {
             getWorld().addObject(transition, 540, 360);
         }
-        if (transition.fadedOnce()) 
-        {
+        if (transition.fadedOnce()) {
             getWorldOfType(LevelWorld.class).stopMusic();
             // Stop the music before transitioning
             // if (getWorld() instanceof ArSYSStartingWorld) {
@@ -46,13 +65,11 @@ public class NextWorld extends CollisionTrigger
                 // ((Tutorial) getWorld()).currentMusic.stop(); // Stop Tutorial music
             // }
             // Transition to the next world
-            if(WorldOrder.nextWorld().equals("Arsys"))
-            {
+            if (WorldOrder.nextWorld().equals("Arsys")) {
                 Greenfoot.setWorld(new Shop(new CutsceneWorld(new ArSYSStartingWorld())));
                 return;
             }
-            if(WorldOrder.nextWorld().equals("Win"))
-            {
+            if (WorldOrder.nextWorld().equals("Win")) {
                 Greenfoot.setWorld(new CutsceneWorld(new MenuWorld()));
                 return;
             }
@@ -64,8 +81,8 @@ public class NextWorld extends CollisionTrigger
             }
         }
     }
-    public String toString()
-    {
-        return(type + "," + rotations + ","  + x + "," + y + "," + triggerNumber);
+
+    public String toString() {
+        return (type + "," + rotations + "," + x + "," + y + "," + triggerNumber);
     }
 }
--- Tutorial.java
+++ Tutorial.java
@@ -12,6 +12,21 @@ public class Tutorial extends LevelWorld
     
     private SuperTextBox textbox;
     private SuperTextBox textbox1;
+    
+    
+    
+    private String[] tutorialMessages = {
+        "Welcome to the tutorial. \n Right arrow to continue",
+        "Use WASD or set them in the Settings Menu \n to move. Right arrow to continue",
+        "Use E to parry, changeable in Settings. \n Right arrow to continue",
+        "Defeat monsters to collect cash for \n upgrades. Right arrow to continue",
+        "To switch weapons, press 1/2/3/4. Right \n arrow to continue",
+        "Hotkey 1 is an automatic, 2 is \n a projectile, 3 is a rocket launcher \n and 4 is a shotgun Right arrow to continue",
+        "Reach as far as possible while not \n dying. Right arrow to continue",
+        "You can trigger the monster spawns to \n practice. Right arrow to continue",
+        "Some places have firewalls. Find keys \n to access past it Right arrow to continue",
+        "Find the portal and press space to move \n into the next level.. Good luck"
+    };
     /**
      * Constructor for objects of class Tutorial.
      * 
@@ -28,7 +43,7 @@ public class Tutorial extends LevelWorld
         WorldOrder.createArrayList();
         WorldOrder.setIndex(levelName);
         setBackground("black.png");
-        TutorialAvatar tutorial = new TutorialAvatar();
+        TutorialAvatar tutorial = new TutorialAvatar(tutorialMessages);
         
         addObject(tutorial, 1080, 650);
     }
